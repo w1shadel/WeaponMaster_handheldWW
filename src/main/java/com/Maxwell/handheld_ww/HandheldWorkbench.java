c4e0e3de0822735640ed69f52670ce7f4dd54e00
w1shadel <kik.souta@gmail.com>
2026-01-06 21:36:06 +0900

    a

--- src/main/java/com/Maxwell/handheld_ww/HandheldWorkbench.java
+++ src/main/java/com/Maxwell/handheld_ww/HandheldWorkbench.java
@@ -1,10 +1,12 @@
 package com.Maxwell.handheld_ww;
 
+import com.sky.weaponmaster.WeaponMaster;
 import net.minecraft.core.registries.Registries;
 import net.minecraft.network.chat.Component;
 import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
+import net.minecraftforge.event.BuildCreativeModeTabContentsEvent;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
@@ -21,8 +23,14 @@ public class HandheldWorkbench
     {
         IEventBus modEventBus = context.getModEventBus();
         ITEMS.register(modEventBus);
+        modEventBus.addListener(this::addCreative);
     }
     public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(Registries.ITEM, MODID);
     public static final RegistryObject<Item> PORTABLE_WORKBENCH = ITEMS.register("portable_workbench",
             () -> new PortableWorkStationItem(new Item.Properties()));
+    private void addCreative(BuildCreativeModeTabContentsEvent event) {
+        if (event.getTab() == WeaponMaster.ccustomTab) {
+            event.accept(PORTABLE_WORKBENCH);
+        }
+    }
 }
